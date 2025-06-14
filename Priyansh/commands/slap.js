6f4c5a7fc8885cd9ccf52ad6be14aa480d0f1e08
Jordanxhks <jordenxhk3@gmail.com>
2025-06-14 06:01:50 +0500

    Update slap.js

--- Priyansh/commands/slap.js
+++ Priyansh/commands/slap.js
@@ -1,4 +1,4 @@
-var mysterious = "Siegfried Sama";
+var mysterious = "JORDANOFFICIAL";
 const request = require("request");
 const fs = require("fs")
 const axios = require("axios")
@@ -7,17 +7,17 @@ module.exports.config = {
   version: "3.0.0",
   hasPermssion: 0,
   credits: `${mysterious}`,
-  description: "it's just imitated because the old slap doesn't work",
-  commandCategory: "...",
+  description: "",
+  commandCategory: "fun",
   usages: "[tag]",
   cooldowns: 5,
 };
 
 module.exports.run = async({ api, event, Threads, global }) => {
-  var link = [ "https://i.postimg.cc/1tByLBHM/anime-slap.gif", ];
+  var link = [ "https://i.imgur.com/9CqzeTe.gif", ];
    var mention = Object.keys(event.mentions);
      let tag = event.mentions[mention].replace("@", "");
     if (!mention) return api.sendMessage("Mention 1 person that you want to slap", threadID, messageID);
-   var callback = () => api.sendMessage({body:`Slapped! ${tag}` + `\n\n*sorry, i thought there's mosquito in ur ugly face*`,mentions: [{tag: tag,id: Object.keys(event.mentions)[0]}],attachment: fs.createReadStream(__dirname + "/cache/slap.gif")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/slap.gif"));  
+   var callback = () => api.sendMessage({body:`‎☆||  ⋆⃝❥͜͡ ${tag}` + `\n\n≪══════◄••❀••►══════≫`,mentions: [{tag: tag,id: Object.keys(event.mentions)[0]}],attachment: fs.createReadStream(__dirname + "/cache/slap.gif")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/slap.gif"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/slap.gif")).on("close",() => callback());
 }
